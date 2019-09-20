29a1298d8dece153400d4b2cba3e62f6c355a982
beteljubs <41305417+beteljubs@users.noreply.github.com>
2019-09-20 16:03:00 -0300

    Operações matemáticas
    Operações matemáticas com e sem o uso da biblioteca math. 
    Exemplo de potência, raiz quadrada, raiz cúbica e divisão.
    Exemplo de usos do .format.

---
+++ math.py
@@ -0,0 +1,22 @@
+import math
+
+x = float(input("Escolha um numero: "))
+y = math.pow(x, 3)  # potência: ao cubo
+
+print("O cubo de {} é {}".format(x, y))
+
+u = float(input"Escolha um numero positivo: ")
+v = float(math.sqrt(u))  # raiz quadrada
+
+print("A raiz quadrada de {} é {}".format(v))
+
+a = float(input("Escolha um numero: "))
+
+# calculo dentro do format e sem precisar da biblioteca math:
+print("A raíz cúbica de {} é {}".format(x, float(x ** (1/3)))
+
+b = float(input("dividendo: "))
+c = float(input("divisor: "))
+
+print("A divisão entre {} e {} é {}".f(x, y, x/y))  # .format como .f
+
