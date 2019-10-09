a650e435355ef7b888c0b2cb728cd35b96fc0e67
Juvs <41305417+beteljubs@users.noreply.github.com>
2019-10-09 09:04:17 -0300

    Perímetro e volume
    Cálculo do perímetro de um quadrado e do volume de uma esfera, com lado e raio lidos.

---
+++ quadrado_esfera.py
@@ -0,0 +1,11 @@
+import math
+
+x = float(input("Escolha o valor do lado do quadrado: "))  
+
+print("O perimetro de um quadrado de {} é {} ".format(float(x, x * 4)))
+
+r = float(input("Escolha o valor do raio de uma esfera: "))
+
+vol = float((4/3) * math.pi * (r ** 3)
+
+print("O volume da esfera de raio {} é {}".format(float(r, vol)))
